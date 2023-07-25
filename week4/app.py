24a069d131eda536a81010760cfd1617e30bf538
millie0808 <102743839+millie0808@users.noreply.github.com>
2023-07-26 01:58:15 +0800

    git commit -m "message"

--- week4/app.py
+++ week4/app.py
@@ -1,29 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
-app=Flask(__name__)
+app=Flask(__name__,static_folder="static",static_url_path="/")
 app.secret_key = 'secret secret key'
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
 def index():
-    session['SIGNED-IN'] = False
-    if request.method == 'POST':
-        if request.values['send'] == '登入':
-            session['account']=request.form.get('account')
-            session['password']=request.form.get('password')
-            return redirect(url_for('signin'))
-    else:
-	    return render_template('home.html')
+    return render_template('home.html')
 
-@app.route('/signin')
+@app.route('/signin', methods=['POST'])
 def signin():
-    account = session.get('account') 
-    password = session.get('password')
+    account = request.form.get('account')
+    password = request.form.get('password')
     if account == "" or password == "":
+        session['SIGNED-IN'] = False
         return redirect(url_for('login_error', message='請輸入帳號密碼'))
     elif account == 'test' and password == 'test':
         session['SIGNED-IN'] = True
         return redirect(url_for('login_success'))
     else:
+        session['SIGNED-IN'] = False
         return redirect(url_for('login_error', message='帳號或密碼輸入錯誤'))
 
 @app.route('/member')
@@ -41,9 +36,15 @@ def login_error():
 
 @app.route('/signout', methods=['GET'])
 def signout():
-    if request.method == 'GET':
-        session['SIGNED-IN'] = False
-        return redirect(url_for('index'))
+    session['SIGNED-IN'] = False
+    return redirect(url_for('index'))
+
+@app.route('/square/<pNumber>')
+def square(pNumber):
+    pNumber = int(pNumber)
+    result = pNumber * pNumber
+    return render_template('result.html',result=result)
+
 
 if __name__ == '__main__':
 	app.run(port='3000',debug=True)
